4550f6a40d450c1f447bc99f93881107ef26e1c1
ldt2711 <ldt2711@gmail.com>
2025-09-29 14:29:06 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuanLySinhVien_BTL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
