ad00a6457255b702719b85fd3a61c0ad7111f6c1
thuong <buiminhthuong30122005@gmail.com>
2025-10-10 23:38:09 +0700

    Add controller, view cho home

---
+++ Areas/Lecturer/Controllers/FAQ.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLySinhVien_BTL.Areas.Lecturer.Controllers
+{
+    public class FAQ : Controller
+    {
+        public class FAQController : Controller
+        {
+            [Area("Lecturer")]
+            public IActionResult Index()
+            {
+                return View();
+            }
+        }
+    }
+}
