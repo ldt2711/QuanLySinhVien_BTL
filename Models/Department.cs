79d0db61578a8b1f8c94b70611c4550d9184350c
thuong <buiminhthuong30122005@gmail.com>
2025-11-02 13:54:26 +0700

    2

--- Models/Department.cs
+++ Models/Department.cs
@@ -11,5 +11,6 @@ namespace QuanLySinhVien_BTL.Models
         public DateTime DateOfEstablishment { get; set; }
 
         public ICollection<Lecturer> Lecturers { get; set; } = new List<Lecturer>();
+        public ICollection<Major> Majors { get; set; } = new List<Major>();
     }
 }
