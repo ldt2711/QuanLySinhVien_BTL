e4aff499f8beae292501a1928893a08e26b54b1d
ldt2711 <ldt2711@gmail.com>
2025-11-07 03:00:02 +0700

    Password no longer require digit

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
     options.Password.RequireUppercase = false;
     options.Password.RequireNonAlphanumeric = false;
     options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = false;
 }
 ).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
